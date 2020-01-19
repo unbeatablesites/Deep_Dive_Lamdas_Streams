696d599f724219d61bce2649a84fbbe077e9d1fe
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-19 00:54:03 -0500

    Lamdas And Streams Practice

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -5,4 +5,10 @@ public class Book {
     private String authorLName;
     private int pages;
 
+    public Book(String title, String authorFName, String authorLName, int pages) {
+        this.title = title;
+        this.authorFName = authorFName;
+        this.authorLName = authorLName;
+        this.pages = pages;
+    }
 }
