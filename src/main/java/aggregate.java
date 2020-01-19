c845f26bc95d501e6facf046af67d371198f9371
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-19 01:11:31 -0500

    Lamdas And Streams Practice

--- src/main/java/aggregate.java
+++ src/main/java/aggregate.java
@@ -1,8 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class aggregate {
 
-    Book book1 = new Book("Miss Peregrines's Home for Peculiar Children","Ranson","Riggs" , 382);
-    Book book2 = new Book("Harry Potter And the sorcerers Stone","JK","Rowling" , 411);
-    Book book3 = new Book("The Cat in the Hat","Dr","Seuss" , 45);
+    public static void main(String[] args) {
+        Book book1 = new Book("Miss Peregrines's Home for Peculiar Children","Ranson","Riggs" , 382);
+        Book book2 = new Book("Harry Potter And the sorcerers Stone","JK","Rowling" , 411);
+        Book book3 = new Book("The Cat in the Hat","Dr","Seuss" , 45);
 
+        List<Book> bookList = Arrays.asList(book1,book2,book3);
+
+        int total = bookList.stream()
+                .collect(Collectors.summingInt(Book::getPages));
+
+    }
 
 }
