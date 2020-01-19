f2ef5eaea46d6a9c1bb49e425ef228dbf9ccf7a5
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-19 01:01:21 -0500

    Lamdas And Streams Practice

--- src/main/java/aggregate.java
+++ src/main/java/aggregate.java
@@ -1,6 +1,7 @@
 public class aggregate {
 
-    Book book1 = new Book("Miss Peregrines's Home for Peculiar Cheldren","Ranson","Riggs" , 382);
+    Book book1 = new Book("Miss Peregrines's Home for Peculiar Children","Ranson","Riggs" , 382);
+    Book book2 = new Book("Harry Potter And the sorcerers Stone","JK","Rowling" , 411);
 
 
 }
